9e9fa5faeaa6b26d0bea4e6449429b59453740d7
Curtis Rutland <curtisrutland@gmail.com>
2018-02-11 21:22:04 -0600

    editing and deleting items

--- app/src/main/java/com/curtisrutland/tabletoptrackr/adapters/CombatEventRecyclerAdapter.kt
+++ app/src/main/java/com/curtisrutland/tabletoptrackr/adapters/CombatEventRecyclerAdapter.kt
@@ -5,9 +5,17 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.PopupMenu
 import android.widget.TextView
 import com.curtisrutland.tabletoptrackr.R
+import com.curtisrutland.tabletoptrackr.controllers.AddCombatEventActivity
 import com.curtisrutland.tabletoptrackr.models.CombatEvent
+import com.curtisrutland.tabletoptrackr.services.CombatEventDataService
+import org.jetbrains.anko.alert
+import org.jetbrains.anko.noButton
+import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.yesButton
 
 class CombatEventRecyclerAdapter(private val context: Context, private val events: List<CombatEvent>)
     : RecyclerView.Adapter<CombatEventRecyclerAdapter.Holder>() {
@@ -26,16 +34,53 @@ class CombatEventRecyclerAdapter(private val context: Context, private val event
     }
 
     inner class Holder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
+
         private val source = itemView?.findViewById<TextView>(R.id.sourceText)
         private val hpChange = itemView?.findViewById<TextView>(R.id.hpChangeText)
         private val currentHp = itemView?.findViewById<TextView>(R.id.currentHpText)
         private val notes = itemView?.findViewById<TextView>(R.id.notesText)
+        private val icon = itemView?.findViewById<ImageView>(R.id.menuIcon)
+        private val arrow = itemView?.findViewById<ImageView>(R.id.arrowIcon)
+        private val editMenuItemId = R.id.editMenuItem
+        private val deleteMenuItemId = R.id.deleteMenuItem
 
         fun bindCombatEvent(event: CombatEvent) {
             source?.text = event.source
             hpChange?.text = event.hpChange?.toString() ?: "0"
             currentHp?.text = event.currentHp.toString()
             notes?.text = event.notes
+
+            val hpVis = if (event.hpChange ?: 0 == 0) View.GONE else View.VISIBLE
+
+            hpChange?.visibility = hpVis
+            currentHp?.visibility = hpVis
+            arrow?.visibility = hpVis
+
+            icon?.setOnClickListener {
+                val popup = PopupMenu(context, icon)
+                popup.menuInflater.inflate(R.menu.popup_menu, popup.menu)
+                popup.setOnMenuItemClickListener { item ->
+                    when (item.itemId) {
+                        editMenuItemId -> {
+                            context.startActivity<AddCombatEventActivity>(
+                                    "source" to event.source,
+                                    "notes" to event.notes,
+                                    "hpChange" to event.hpChange.toString(),
+                                    "id" to event.id.toString()
+                            )
+                        }
+                        deleteMenuItemId -> {
+                            context.alert("Are you sure you want to remove this event?",
+                                    "Delete Event?") {
+                                yesButton { CombatEventDataService.removeEvent(event.id) }
+                                noButton { }
+                            }.show()
+                        }
+                    }
+                    true
+                }
+                popup.show()
+            }
         }
     }
 }
